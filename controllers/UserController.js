7cbba18fb1972694c6cb6e4077ee3432db24f5c8
RodrigoPresa <rodrigo_presa@outlook.com>
2021-04-27 15:05:21 -0300

    add file upload methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,19 +9,61 @@ class UserController {
     onSubmit(){
         this.formEl.addEventListener("submit", e =>{
             e.preventDefault();
+
+            let values = this.getValues()
             
-            this.addLine(this.getValues());
-        })
+            this.getPhoto().then((content)=>{
+                values.photo = content;
+                this.addLine(values);
+            },(e)=>{
+                console.error(e);
+            });
+            
+        });
+    }
+
+    getPhoto(){
+
+        return new Promise((resolve,reject)=>{
+            let fileReader = new FileReader;
+            let elements = [...this.formEl.elements].filter(item=>{
+                if (item.name === 'photo'){
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+            
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e)=>{
+                reject(e);
+            };
+            if(file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/boxed-bg.jpg');
+            }
+            
+        });
+
+        
     }
 
     getValues(){
         let user = {};
-        this.formEl.elements.forEach((field, index)=>{
+        [...this.formEl.elements].forEach((field, index)=>{
             
             if(field.name == "gender"){
                 if(field.checked){
                     user[field.name] = field.value;
-                }        
+                }  
+            }else if (field.name == "admin"){
+                user[field.name] = field.checked;
             }else {
                 user[field.name] = field.value;
             }
@@ -40,19 +82,20 @@ class UserController {
     }
     
     addLine(users){
+
+        let tr = document.createElement('tr');
         
-        this.tableEl.innerHTML = `
-                <tr>
-                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                    <td>${users.name}</td>
-                    <td>${users.email}</td>
-                    <td>${users.admin}</td>
-                    <td>${users.birth}</td>
-                    <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                </tr>
+        tr.innerHTML = `
+            <td><img src="${users.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${users.name}</td>
+            <td>${users.email}</td>
+            <td>${(users.admin) ? "Sim" : "Não"}</td>
+            <td>${users.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         `;
+        this.tableEl.appendChild(tr);
     }
 }
